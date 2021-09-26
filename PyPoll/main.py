b4f8c841b32cd9807fa458f88baa062972e4cb14
melorebecca <melorebecca76@gmail.com>
2021-09-26 17:33:09 -0400

    Updated definition,  varibales

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,20 +4,38 @@ import csv
 #file path
 csvpath = os.path.join('..','PyPoll','Resources', 'election_data.csv')
 
+#define
+def Poll(data):
 
-#variables
-voterID = []
-total_votes = 0
-canidate_votes = {}
-canidate_percent = {}
-win_votes = 0
-wiiner = []
+        #variables
+        total_votes = 0
+        vote_count = []
+        vote_percent []
+        canidate_list = []
+        unique_canidate = []
+        winner = []
+
+        #loop
+        for row in data:
+
+                #sum of votes
+                total_votes += 1
+
+                #append canidate names
+                if row[2] not in unique_canidate:
+                        unique_canidate.append(row[2])
+                
+                #new list
+                votes.append(row[2])
+
+
+
+
+
+
 
 
 # read the csv
 with open(csvpath, 'r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         csvheader = next(csvreader)
-
-        # loop 
-        for row in csvreader:
