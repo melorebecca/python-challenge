b588ddca5e4381ba2d8720872a997a3e5f5f2633
melorebecca <melorebecca76@gmail.com>
2021-09-26 18:00:27 -0400

    Updated print

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,12 +35,32 @@ def Poll(data):
                 #winner calc
                 winner = unique_canidate[canidates.index(max(canidates))]
 
+                #print 
+                print ("Election Results")
+                print ("-------------------------")
+                print ("Total Votes: " + str(total_votes))
+                print ("-------------------------")
+                for i in range(len(unique_canidates)):
+                        print (f'{unique_canidates[i]: {total_percent[i]}% {Candidates[i]}')
+                print ("-------------------------")
+                print(f"Winner: {winner}")
+                print ("-------------------------"
 
+# write
+output_file = os.path.join('..','PyPoll','Analysis', "election_analysis.txt")
 
-
-
-
-
+with open(output_file, "w") as text:
+    text.write("Election Results" + "\n")
+    text.write("-------------------------" + "\n")
+    text.write("Total Votes: " + str(votes) + "\n")
+    text.write("-------------------------" + "\n")
+    text.write(f"Khan: {round((khan_votes/votes*100),6)}% ({khan_votes})"+ "\n")
+    text.write(f"Correy: {round((correy_votes/votes*100),6)}% ({correy_votes})"+ "\n")
+    text.write(f"Li: {round((li_votes/votes*100),6)}% ({li_votes})"+ "\n")
+    text.write(f"O'Tooley: {round((otooley_votes/votes*100),6)}% ({otooley_votes})"+ "\n")
+    text.write("-------------------------" + "\n")
+    text.write(f"Winner: {winner}" + "\n")   
+    text.write("-------------------------" + "\n")
 
 
 
